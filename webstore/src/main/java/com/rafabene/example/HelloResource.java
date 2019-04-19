a5729e647a54343de40a383147b7e811868189ea
Rafael Benevides <rafabene@gmail.com>
2019-04-19 19:39:08 -0300

    Changed hello message

--- webstore/src/main/java/com/rafabene/example/HelloResource.java
+++ webstore/src/main/java/com/rafabene/example/HelloResource.java
@@ -11,6 +11,6 @@ public class HelloResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return "v2 - hello";
     }
 }
--- webstore/src/test/java/com/rafabene/example/HelloResourceTest.java
+++ webstore/src/test/java/com/rafabene/example/HelloResourceTest.java
@@ -15,7 +15,7 @@ public class HelloResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(is("v2 - hello"));
     }
 
 }
