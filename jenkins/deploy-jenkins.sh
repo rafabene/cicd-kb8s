72a4be5e11866b57f699150ae89b89c644887a42
Rafael Benevides <rafabene@gmail.com>
2020-03-18 20:15:13 -0300

    Updated GraalVM and Jenkins deployment method

--- jenkins/deploy-jenkins.sh
+++ jenkins/deploy-jenkins.sh
@@ -1,8 +1,6 @@
 eval $(minikube docker-env)
-kubectl apply -f Service.yaml
-export KB8IP=`kubectl get services -n cicd|grep jenkins|awk  '{printf $3}'`
-envsubst < config.xml.template > config.xml
 docker build -t rafabene/myjenkins .
 kubectl delete pods -n cicd -l app=myjenkins --force --grace-period=0 || echo "Deploy jenkins using deploy-kb8s.sh"
 kubectl apply -f Deployment.yaml
+kubectl apply -f Service.yaml
 kubectl create rolebinding -n cicd my-sa-admin --clusterrole=admin --serviceaccount=cicd:default || echo "Security already configured"
